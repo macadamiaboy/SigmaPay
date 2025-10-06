22cb4aa1e624f4289eee2330d5fdcd36ceef2ac1
elknu <elknumber1@yandex.com>
2025-10-07 01:29:42 +0300

    Get and delete methods added

--- internal/postgres/tables-methods/helpers.go
+++ internal/postgres/tables-methods/helpers.go
@@ -21,3 +21,19 @@ func SaveHelper(db *sql.DB, env string, query string, structFields ...any) error
 
 	return nil
 }
+
+func DeleteByIDHelper(db *sql.DB, env string, query string, id int64) error {
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("%s: failed to prepare the stmt, err: %v", env, err)
+		return fmt.Errorf("%s: failed to prepare the stmt, err: %w", env, err)
+	}
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		log.Printf("%s: failed to exec the stmt, err: %v", env, err)
+		return fmt.Errorf("%s: %w", env, err)
+	}
+
+	return nil
+}
--- internal/postgres/tables-methods/pricelist.go
+++ internal/postgres/tables-methods/pricelist.go
@@ -2,9 +2,12 @@ package tablesmethods
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 )
 
 type EventType struct {
+	Id    int64
 	Type  string
 	Price int
 }
@@ -14,4 +17,33 @@ func (e *EventType) Save(db *sql.DB) error {
 	query := "INSERT INTO pricelist(type, price) VALUES($1, $2);"
 
 	return SaveHelper(db, env, query, e.Type, e.Price)
+}
+
+func (e *EventType) GetByID(db *sql.DB, id int64) (*EventType, error) {
+	env := "postgres.tables-methods.pricelist.GetByID"
+
+	stmt, err := db.Prepare("SELECT * FROM pricelist WHERE id = $1")
+	if err != nil {
+		log.Printf("%s: failed to prepare the stmt, err: %v", env, err)
+		return nil, fmt.Errorf("%s: failed to prepare the stmt, err: %w", env, err)
+	}
+
+	var idOfEvent int64
+	var typeOfEvent string
+	var priceOfEvent int
+	err = stmt.QueryRow(id).Scan(&idOfEvent, &typeOfEvent, &priceOfEvent)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", env, err)
+	}
+
+	var res EventType = EventType{Id: idOfEvent, Type: typeOfEvent, Price: priceOfEvent}
+
+	return &res, nil
+}
+
+func (e *EventType) DeleteByID(db *sql.DB, id int64) error {
+	env := "postgres.tables-methods.pricelist.DeleteByID"
+	query := "DELETE FROM pricelist WHERE id = $1;"
+
+	return DeleteByIDHelper(db, env, query, id)
 }
